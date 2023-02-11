537078f9019900414e260a439dbb9c61cbdff997
unknown <gushcha1128@gmail.com>
2023-02-12 02:16:49 +0300

    feat: update  index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 module.exports = function reverse (n) {
+    /* third solition = was interested how could reverse the num if its length -  more then 4 (not like in test js) */
+    
+    if (n < 0) {n = Math.abs(n)};
+    const nStr = n.toString();
+    let result = [];
+    for (let i = nStr.length; i > 0; i--) {
+        if (result.length != nStr.length) { result.push(nStr[((nStr.length + i) - (nStr.length + 1))]) }
+        }
+    return result = Number(result.join(''));
+    
+}
+/* second solution after getting more knowlege )
+
+    if (n < 0) {n = Math.abs(n)};
+    return Array.from(n.toString()).reverse().join(''); */
+
+// first solution 
+
+/*   function reverse (n) {
     if (n < 0) {n = Math.abs(n)};
     const nStr = n.toString(); 
     const resultArr = [];
@@ -6,8 +25,5 @@ module.exports = function reverse (n) {
     const resultStr = resultArr.join('');
     return Number(resultStr);
     
-}
-/* another soluthion after getting more knowlege )
+}*/
 
-    if (n < 0) {n = Math.abs(n)};
-    return Array.from(n.toString()).reverse().join(''); */
