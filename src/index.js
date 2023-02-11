74186d170c90b0a910caad924104a24508742eae
unknown <gushcha1128@gmail.com>
2023-02-11 23:47:59 +0300

    feat: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {n = Math.abs(n)};
+    const nStr = n.toString(); 
+    const resultArr = [];
+    resultArr.push(nStr[2] + nStr[1] + nStr[0]);
+    const resultStr = resultArr.join('');
+    return Number(resultStr);
+    
 }
+/* another soluthion after getting more knowlege )
+
+    if (n < 0) {n = Math.abs(n)};
+    return Array.from(n.toString()).reverse().join(''); */
